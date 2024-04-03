be3226f34ceeed5a14e733f07be6a5e5f6a148ed
abcbyuman <95644079+abcbyuman@users.noreply.github.com>
2024-04-02 22:29:58 -0600

    Exporting csv

--- lema1.py
+++ lema1.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import csv
 
 url = "https://lamadeleine.com/wp-json/wp/v2/restaurant-locations?per_page=150"
 headers = {
@@ -9,7 +10,6 @@ response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
 
 def extract_location_info(locations):
     extracted_info = []
@@ -26,9 +26,25 @@ def extract_location_info(locations):
         extracted_info.append(info)
     return extracted_info
 
-# Use the function to parse the JSON data
+#using the function to parse the JSON data
 location_info = extract_location_info(data)
 
-# Print the extracted information
+#print extracted info
 for info in location_info:
     print(json.dumps(info, indent=2))
+
+print(f"Total number of store locations found: {len(location_info)}")
+
+csv_file_name = 'api_store_details.csv'
+
+fieldnames = ['locationName', 'streetAddress', 'city', 'state', 'postalCode', 'phoneNumber', 'storeID']
+
+#writing to the CSV file
+with open(csv_file_name, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    #writing rows
+    for location in location_info:
+        writer.writerow(location)
+
+print(f"Data successfully written to {csv_file_name} in the Selenium directory")
