defd2a3df035ff07123fd11796ac41786851a470
abcbyuman <95644079+abcbyuman@users.noreply.github.com>
2024-04-02 19:57:42 -0600

    Comments

--- lema.py
+++ lema.py
@@ -8,16 +8,23 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 
-#turning on headless mode to speed up process
-options = Options()
-options.headless = True
+#There are 3 sections: 
+#1- Loading the given website and scraping store data 
+#2- Clicking on link to navigate to store location address, and then scraping the address information
+#3- Wrangling the data saved from the first 2 steps into a single, cleaned dataframe that can then be used to incorporate the review data and perform analytics
 
+
+
+#STEP 1
+
+#configuring webdriver
 service = Service('/Users/austin/Downloads/chromedriver-mac-x64/chromedriver')
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(service=service)
 
 #go to lamadeleline webpage
 driver.get(f'https://lamadeleine.com/locations')
 
+#I use this time to manually zoom out on the map in browser (click the zoom out button on bottom right of page 8 times). This is needed to scrape ALL locations, not just the ones that show up as default
 time.sleep(3)
 
 #saving handle of original window, for use later
@@ -34,35 +41,31 @@ for element in elements:
     #print(element.text)
     elements_texts.append(element.text)
 
-#print(len(elements_texts))
-
-#print(elements_texts)
-
 total_restaurants = 0
 
-# Iterate over each string in elements_texts
+#iterating through text in my list and counting how many restaurants were scraped
 for text in elements_texts:
-    # Count occurrences of a unique marker for each restaurant
-    # Assuming 'Get Directions\nCafé Details' marks the end of each restaurant's details
+    #count occurrences of unique marker for each restaurant
     num_restaurants = text.count('Get Directions\nCafé Details')
     total_restaurants += num_restaurants
 
 print("Number of restaurants:", total_restaurants)
 
-#print('MOVING ON TO ADDRESS')
 
 
-directions_links = driver.find_elements(By.CSS_SELECTOR, ".location__directions")
 
-#initiate counter and list
+# STEP 2
+
+
+#initiate counter and list (counter will just help with debugging)
 i = 1
 locations = []
 
-# Get the count of direction links to iterate over
+#find the number of links I need to iterate over
 num_of_directions = len(driver.find_elements(By.CSS_SELECTOR, ".location__directions"))
 
 for index in range(num_of_directions):
-    # Refind the direction links each time to avoid stale elements
+    #refind the "directions" links each time
     directions_links = driver.find_elements(By.CSS_SELECTOR, ".location__directions")
     link = directions_links[index]
 
@@ -70,28 +73,30 @@ for index in range(num_of_directions):
     link.click()
     driver.switch_to.window([window for window in driver.window_handles if window != original_window][0])
 
-    #manually solution for getting destination element. I ran into issues where it would instead grab the starting point address.
+    #solution for getting destination element. I ran into issues where it would grab the starting point address instead of the destination address.
     aria_controls_selector = "input[aria-controls='sbsg51']"    
 
-    #wait for page to load
+    #wait for element we are after to load, up to 20 sec
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.tactile-searchbox-input')))
 
-    #using my manual solution for locating correct element to navigate
+    #using destination address variable to ensure we get the html element
     address_element = driver.find_element(By.CSS_SELECTOR, aria_controls_selector)
 
     #retrieve attribute that we are after and print
     address_value = address_element.get_attribute('aria-label')
+
+    #this print helps debug 
     print("Address " + str(i) + ": " + str(address_value))
-    locations.append(address_value)
-    #increment counter
+    #(debugging)
     i = i + 1
+
+    #add each new address to the address array
+    locations.append(address_value)
+   
     
     #closing tab and switching back to main tab
     driver.close()
     driver.switch_to.window(original_window)
-
-    #adding delay to avoid being detected as a bot/not overload the server
-    #time.sleep(2)
 
 #standardize delimiter in store_details list, this will help when turning it into a df
 store_details = [text.replace('\n', ', ') for text in elements_texts]
@@ -104,7 +109,10 @@ print (locations)
 driver.quit()
 
 
-#Wrangling Data
+
+
+
+# STEP 3
 #I will use pandas to manipulate the data
 
 #create df for locations
@@ -133,7 +141,7 @@ state_abbreviations = {
     "Texas": "TX",
     "California": "CA",
     "Maryland": "MD",
-    # Add other states as necessary
+    #other states can be added as necessary
 }
 
 #function to replace state names with abbreviations
@@ -146,48 +154,44 @@ def abbreviate_state(address, state_map):
 #call function
 store_details_df['Address'] = store_details_df['Address'].apply(abbreviate_state, state_map=state_abbreviations)
 
-#remove "destination" from the data to prepare for splitting address column
+#remove "destination" from the Address column data to prepare for splitting the column into smaller parts
 store_details_df['Address'] = store_details_df['Address'].str.replace('Destination ', '')
 print(store_details_df)
 
 
-
+#splitting Address column into smaller parats
 def split_address(row):
-    # Count the number of commas in the Address
+    #count the number of commas in the Address
     address = row['Address']
     num_commas = row['Address'].count(',')
-    print(num_commas)
-    print("break")
-    # if num_commas == 3:
-    #     address = address.split(', ', 1)[1]
+    #debugging code
+    # print(num_commas)
+    # print("break")
 
+    #this conditional helps clean the data. Some entries are missing street address, this accounts for that
     if num_commas == 1:
-        # If there is only 1 comma, leave the streetAddress column blank
+        #if only 1 comma, leave streetAddress column blank
         row['city'], row['state_postalCode'] = row['Address'].rsplit(', ', maxsplit=1)
         row['streetAddress'] = ""  # Set streetAddress to blank
     else:
-        # If there are 2 commas, proceed as normal
+        #if 2 commas, proceed with splitting (this is the expected # of commas)
         row['streetAddress_city'], row['state_postalCode'] = row['Address'].rsplit(', ', maxsplit=1)
         row['streetAddress'], row['city'] = row['streetAddress_city'].rsplit(', ', maxsplit=1)
-    # else:
-    #     print(num_commas)
-    #     print("comma detection error")
     
-    # Split state_postalCode into state and postalCode by assuming state is always 2 characters
+    #split state_postalCode into state and postalCode by assuming state is always 2 characters
     row['state'] = row['state_postalCode'][:2]
     row['postalCode'] = row['state_postalCode'][2:]
 
     return row
 
-# Assuming 'Address' column has been prepared (with 'Destination ' removed)
-# Apply the function to each row
+#apply function to each row in df
 store_details_df = store_details_df.apply(split_address, axis=1)
 
 
-# Drop the intermediate columns
+#drop unnecesssary columns
 store_details_df.drop(columns=['Address', 'streetAddress_city', 'state_postalCode'], inplace=True)
 
-
+#re order columns for ease of viewing
 columns = list(store_details_df.columns)
 new_columns_order = columns[1:-2] + [columns[0]] + columns[-2:]
 store_details_df = store_details_df[new_columns_order]
